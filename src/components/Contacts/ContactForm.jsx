0f2a737836048c693929926ba479aa8fd6aa6b57
user.email <jobayer.ahmed109@gmail.com>
2024-02-16 14:55:42 +0600

    Now form is under controlled and data successfully submitted

--- src/components/Contacts/ContactForm.jsx
+++ src/components/Contacts/ContactForm.jsx
@@ -1,1 +1,50 @@
+import { useState } from "react";
+import shortid from "shortid";
+import InputField from "./InputField";
 
+const CONTACT_INIT_STATE = {
+  id: shortid.generate(),
+  name: "",
+  email: "",
+};
+
+const ContactForm = () => {
+  const [contact, setContact] = useState({ ...CONTACT_INIT_STATE });
+
+  const handleChange = (e) => {
+    setContact({
+      ...contact,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(contact);
+    setContact(CONTACT_INIT_STATE);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <InputField
+        label="Name:"
+        type="text"
+        name="name"
+        value={contact.name}
+        onChange={handleChange}
+      />{" "}
+      <br />
+      <InputField
+        label="Email:"
+        type="email"
+        name="email"
+        value={contact.email}
+        onChange={handleChange}
+      />{" "}
+      <br />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default ContactForm;
