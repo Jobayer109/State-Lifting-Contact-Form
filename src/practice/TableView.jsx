c55a3c2ce45b8eda7025af1526df56dcacec6bb5
user.email <jobayer.ahmed109@gmail.com>
2024-02-19 13:52:25 +0600

    Table content filtered successfully

--- src/practice/TableView.jsx
+++ src/practice/TableView.jsx
@@ -1,25 +1,55 @@
 /* eslint-disable react/prop-types */
 
+import { useState } from "react";
+
 const TableView = ({ contacts }) => {
+  const [filter, setFilter] = useState("all");
+
+  let filteredContacts = [];
+  if (filter === "all") {
+    filteredContacts = contacts;
+  } else {
+    filteredContacts = contacts.filter((contact) => contact.group === filter);
+  }
+  console.log(filteredContacts);
+
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Email</th>
-          <th>Group</th>
-        </tr>
-      </thead>
-      <tbody>
-        {contacts.map((contact, index) => (
-          <tr key={index}>
-            <td>{contact.name}</td>
-            <td>{contact.email}</td>
-            <td>{contact.group}</td>
+    <>
+      <div>
+        <label>Filter</label>
+        <select
+          name="group"
+          id="group"
+          value={filter}
+          onChange={(e) => setFilter(e.target.value)}
+        >
+          <option value="">None</option>
+          <option value="all">All</option>
+          <option value="Office">Office</option>
+          <option value="Home">Home</option>
+          <option value="Family">Family</option>
+          <option value="Business">Business</option>
+        </select>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Group</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {filteredContacts.map(({ name, email, group }, index) => (
+            <tr key={index}>
+              <td>{name}</td>
+              <td>{email}</td>
+              <td>{group}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 };
 
